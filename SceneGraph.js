718541cff65397392a38513a3f0df2ece87c4c50
samme <samme@users.noreply.github.com>
2017-03-23 21:48:14 -0700

    1.0.4

--- SceneGraph.js
+++ SceneGraph.js
@@ -1,6 +1,6 @@
 
 /*
-  Scene Graph plugin 1.0.2 for Phaser
+  Scene Graph plugin 1.0.4 for Phaser
  */
 
 (function() {
@@ -126,7 +126,7 @@
       28: "VIDEO"
     };
 
-    SceneGraph.VERSION = "1.0.2";
+    SceneGraph.VERSION = "1.0.4";
 
     SceneGraph.addTo = function(game) {
       return game.plugins.add(this);
